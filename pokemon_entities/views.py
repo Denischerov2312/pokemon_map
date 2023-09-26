036392b7020bf973cf786b33b0cfbc455aefcb00
Denischerov2312 <hshshsdbhshs@gmail.com>
2023-09-27 00:48:06 +0300

    Сократил код

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -34,14 +34,13 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 def show_all_pokemons(request):
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     pokemons = list(Pokemon.objects.all())
-    for pokemon in pokemons:
-        for pokemon_entity in list(PokemonEntity.objects.filter(pokemon=pokemon)):
-            if pokemon_entity.disappeared_at > localtime() and pokemon_entity.appeared_at < localtime():
-                add_pokemon(
-                    folium_map, pokemon_entity.lat,
-                    pokemon_entity.lon,
-                    request.build_absolute_uri(pokemon.image.url)
-                )
+    for pokemon_entity in list(PokemonEntity.objects.all()):
+        if pokemon_entity.disappeared_at > localtime() and pokemon_entity.appeared_at < localtime():
+            add_pokemon(
+                folium_map, pokemon_entity.lat,
+                pokemon_entity.lon,
+                request.build_absolute_uri(pokemon_entity.pokemon.image.url)
+            )
 
     pokemons_on_page = []
     for pokemon in pokemons:
